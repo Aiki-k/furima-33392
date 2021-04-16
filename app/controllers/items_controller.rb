b81bf0e8362209bf1614e499a7bfb58aeb1d6a86
aiki <aiki.kage@gmail.com>
2021-04-17 01:26:05 +0900

    編集機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user == current_user
+      render :edit
+    else
+      redirect_to item_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
